48ec81a56cdabef0061c757d57e1da89ee0f0687
Vinicius Lira <onilira@gmail.com>
2016-03-23 20:54:28 -0300

    Test that the admin attribute cannont be updated via web or patch requests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,7 +43,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
-  test "shoud redirect destroy when not logged in" do
+  test "should redirect destroy when not logged in" do
     assert_no_difference 'User.count' do
       delete :destroy, id: @user
     end
@@ -56,6 +56,15 @@ class UsersControllerTest < ActionController::TestCase
       delete :destroy, id: @user
     end
     assert_redirected_to root_url
+  end
+
+  test "should not allow te admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { password: '',
+                                            password_confirmation: '',
+                                            admin: true }
+    assert_not @other_user.reload.admin?
   end
 
 end
