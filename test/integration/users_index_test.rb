8ea7e583fb38ab11846d098ad4f6ff7545a44d2c
Vinicius Lira <onilira@gmail.com>
2016-03-22 21:00:11 -0300

    Adds admin attributo to users so it can delete other users

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,17 +3,25 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:exemplar)
+    @admin = users(:exemplar)
+    @non_admin = users(:moral)
   end
 
-  test "index including pagination" do
-    log_in_as(@user)
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
     get users_path
     #puts @response.body
     assert_template 'users/index'
     assert_select 'div.pagination'
-    User.paginate(page: 1, per_page: 3).each do |user|
+    first_page_of_users = User.paginate(page: 1, per_page: 3)
+    first_page_of_users.each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
     end
   end
 end
