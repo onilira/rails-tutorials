97a1d4f06f9b7c4787fc7e7b9a621d93f7fd09e1
Vinicius Lira <onilira@gmail.com>
2016-04-19 23:31:57 -0300

    Spelling correction

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -59,7 +59,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
-  test "should not allow te admin attribute to be edited via the web" do
+  test "should not allow the admin attribute to be edited via the web" do
     log_in_as(@other_user)
     assert_not @other_user.admin?
     patch :update, id: @other_user, user: { password: '',
