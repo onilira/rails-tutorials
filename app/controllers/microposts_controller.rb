637482e6d01672e1959993b41ef26156fca136f8
Vinicius Lira <onilira@gmail.com>
2016-05-10 21:50:41 -0300

    Home page now has a micropost feed

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,8 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost sended!"
       redirect_to root_url
     else
+      @feed_items = []
+      #@feed_items = current_user.feed.paginate(page: params[:page])
       render 'static_pages/home'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -64,6 +64,11 @@ class User < ActiveRecord::Base
 		reset_sent_at < 2.hours.ago
 	end
 
+	# Defines a proto-feed
+	def feed
+		Micropost.where("user_id = ?", id)
+	end
+
 	private
 
 		# Converts email to all lower-case
